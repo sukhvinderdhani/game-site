83b78ab8486f55488256ba7502a47c5a5c1ca31e
Sukhi <sukhvinder.dhani@gmail.com>
2025-02-26 16:04:48 +0530

    Remove duplicated styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton, SkeletonText } from "@/components/ui/skeleton";
 
 const GameCardSkeleton = () => {
   return (
-    <Card.Root width={"300px"} borderRadius={10} overflow="hidden">
+    <Card.Root>
       <Skeleton height={"200px"} />
       <Card.Body>
         <SkeletonText />
